ab44a1130821a158bde8a426b4dd1201e1e13fd1
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-02-20 05:10:25 -0500

    Wrote a python script to randomly color districts
    Updated the GeoJson file to have properties to describe color, and now the color is being read dynamically as one layer leading to a lot faster load times.

--- 416-web-app/src/components/Map/MapBoxComponent.js
+++ 416-web-app/src/components/Map/MapBoxComponent.js
@@ -3,48 +3,7 @@ import ReactMapGL, { Layer, Source } from "react-map-gl"
 //import geoData from '../../data/TestGeoJson.json'
 import geoData from '../../data/NorthCarolinaVotingPrecincts.json'
 
-
-function getRandomColor() {
-  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-  const r = randomBetween(0, 255);
-  const g = randomBetween(0, 255);
-  const b = randomBetween(0, 255);
-  const rgb = `rgb(${r},${g},${b})`;
-  return rgb
-}
-
-function getRandomScalar() {
-  return 1+Math.random()
-}
-
-
-const precincts = []
-
-function populatePrecincts(){
-  /** We will use the enr_desc, which I think is the precinct description, as a unique key to access
-   *  the precincts.
-   */
-
-  for (var index in geoData.features) {
-      var currentFeature = geoData.features[index];
-      precincts.push(
-      <Source
-        id = {currentFeature.properties.enr_desc}
-        type="geojson"
-        data = {geoData.features[index]} />,
-      <Layer
-          id = {currentFeature.properties.enr_desc + "-layer"}
-          type="fill"
-          source={currentFeature.properties.enr_desc}
-          paint={{
-            "fill-color" : ["rgb",["get","fillcolor"], ["get","OBJECTID"], ["get","county_id"]],
-            "fill-opacity": 0.35
-          }}/>)
-  }
-}
-
-
-//populatePrecincts()
+console.log(geoData)
 
 function MapBoxComponent() {
   const [viewport, setViewport] = useState({
@@ -74,7 +33,7 @@ function MapBoxComponent() {
           type="fill"
           source="ncgeodata"
           paint={{
-            "fill-color" : ["rgb",["get","fillcolor"], ["get","OBJECTID"], ["get","county_id"]],
+            "fill-color" : ["rgb",["get","rgb-R"], ["get","rgb-G"], ["get","rgb-B"]],
             "fill-opacity": 0.35
           }}/>
       </ReactMapGL>
