ea3a7fadb5a6ccf375f06e897ba3e06529b75a2f
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-02-20 02:06:14 -0500

    Added district geodata, but it's all scattered
    Need to make a batch script to compile these into one file, but even then it's only for districts and not precincts

--- 416-web-app/src/App.js
+++ 416-web-app/src/App.js
@@ -1,5 +1,5 @@
 import './css/App.css';
-import React, {useState} from "react"
+import React from "react"
 import Toolbar from './components/Toolbar/MapToolbar';
 import MapBoxComponent from './components/Map/MapBoxComponent';
 
