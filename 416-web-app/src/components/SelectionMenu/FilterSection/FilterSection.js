6fff08d9098fdbab7a02a19a68ef7233e3150e2f
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-03-09 23:08:04 -0500

    basic setup for filter stepper added

--- 416-web-app/src/components/SelectionMenu/FilterSection/FilterSection.js
+++ 416-web-app/src/components/SelectionMenu/FilterSection/FilterSection.js
@@ -11,9 +11,6 @@ import Step from '@material-ui/core/Step';
 import StepButton from '@material-ui/core/StepButton';
 import Typography from '@material-ui/core/Typography';
 import ConstraintSelection from './ConstraintSelection'
-
-
-
 import Districting from '../../../utilities/classes/Districting'
 import EnactedDistrictingPlan2011 from '../../../data/NC/EnactedDistrictingPlan2011WithData.json'
 import EnactedDistrictingPlan2016 from '../../../data/NC/EnactedDistrictingPlan2016WithData.json'
@@ -21,32 +18,170 @@ import EnactedDistrictingPlan2019 from '../../../data/NC/EnactedDistrictingPlan2
 import WeightSelection from './WeightSelection'
 import ReturnToMapButton from './ReturnToMapButton'
 
-class FilterSection extends Component {
-    constructor(props) {
-        super(props)
+
+const districtingsToLoad = [
+    new Districting("Enacted Districting Nov 2011 - Feb 2016", EnactedDistrictingPlan2011),
+    new Districting("Enacted Districting Feb 2016 - Nov 2019", EnactedDistrictingPlan2016), 
+    new Districting("Enacted Districting Nov 2019 - Dec 2021", EnactedDistrictingPlan2019)]
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+    completed: {
+      display: 'inline-block',
+    },
+    instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  }));
+  
+  function getSteps() {
+    return ['Select Constraints', 'Apply Weights', 'View Districtings'];
+  }
+  
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return(
+            <ConstraintSelection/>
+        );
+      case 1:
+        return(
+            <WeightSelection/>
+        );
+      case 2:
+        return(
+            <div>Show something useful here. Box and Whisker?</div>
+        );
+      default:
+        return(
+            <div>huh</div>
+        );
     }
+  }
+  
+function FilterSection(props) {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState({});
+    const steps = getSteps();
+  
+    const totalSteps = () => {
+      return steps.length;
+    };
+  
+    const completedSteps = () => {
+      return Object.keys(completed).length;
+    };
+  
+    const isLastStep = () => {
+      return activeStep === totalSteps() - 1;
+    };
+  
+    const allStepsCompleted = () => {
+      return completedSteps() === totalSteps();
+    };
+  
+    const handleNext = () => {
+      const newActiveStep =
+        isLastStep() && !allStepsCompleted()
+          ? // It's the last step, but not all steps have been completed,
+            // find the first step that has been completed
+            steps.findIndex((step, i) => !(i in completed))
+          : activeStep + 1;
+      setActiveStep(newActiveStep);
+    };
+  
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+  
+    const handleStep = (step) => () => {
+      setActiveStep(step);
+    };
+  
+    const handleComplete = () => {
+      const newCompleted = completed;
+      newCompleted[activeStep] = true;
+      setCompleted(newCompleted);
+      handleNext();
+    };
+  
+    const handleReset = () => {
+      setActiveStep(0);
+      setCompleted({});
+    };
 
-    /* Eventually I think it'll be best to load all these JSON files in through network instead of local storage. */ 
-    districtingsToLoad = [
-            new Districting("Enacted Districting Nov 2011 - Feb 2016", EnactedDistrictingPlan2011),
-            new Districting("Enacted Districting Feb 2016 - Nov 2019", EnactedDistrictingPlan2016), 
-            new Districting("Enacted Districting Nov 2019 - Dec 2021", EnactedDistrictingPlan2019)]
-
-    render() {
         return (
-            <div className="SelectionMenuSection FilterSection">
-                {/* Button for returning to map */}
-                <ReturnToMapButton/>
-                {/* Section for Setting Objective Function Weights */}
-                <WeightSelection/>
-                {/* Section for Setting Constriants */}
-                <ConstraintSelection/>
-                {/* Button to apply the filters */}
-                <Button className="redBrownBtn" onClick={(e) => this.props.loadInDistrictings(this.districtingsToLoad)}>{SelectionMenuUtilities.LABELS.APPLY_JOB}</Button>
-            </div>
+        <div className="SelectionMenuSection FilterSection">
+           {/* Button for returning to map */}
+            <ReturnToMapButton/>
+            <div className={classes.root}>
+                <Stepper nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => (
+                    <Step key={label}>
+                        <StepButton onClick={handleStep(index)} completed={completed[index]}>
+                        {label}
+                        </StepButton>
+                    </Step>
+                    ))}
+                </Stepper>
+                <div>
+                    {allStepsCompleted() ? (
+                    <div>
+                        <Typography className={classes.instructions}>
+                        All steps completed - you&apos;re finished
+                        </Typography>
+                        <Button onClick={handleReset}>Reset</Button>
+                    </div>
+                    ) : (
+                    <div>
+                        <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                        <div>
+                        <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                            Back
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleNext}
+                            className={classes.button}
+                        >
+                            Next
+                        </Button>
+                        {activeStep !== steps.length &&
+                            (completed[activeStep] ? (
+                            <Typography variant="caption" className={classes.completed}>
+                                Step {activeStep + 1} already completed
+                            </Typography>
+                            ) : (
+                            <Button variant="contained" color="primary" onClick={handleComplete}>
+                                {completedSteps() === totalSteps() - 1 ? 'Finish' : 'Complete Step'}
+                            </Button>
+                            ))}
+                        </div>
+                    </div>
+                    )}
+                </div>
+                </div>
+                </div>
+            // <div className="SelectionMenuSection FilterSection">
+            //     {/* Button for returning to map */}
+            //     <ReturnToMapButton/>
+            //     {/* Section for Setting Objective Function Weights */}
+            //     <WeightSelection/>
+            //     {/* Section for Setting Constriants */}
+            //     <ConstraintSelection/>
+            //     {/* Button to apply the filters */}
+            //     <Button className="redBrownBtn" onClick={(e) => props.loadInDistrictings(this.districtingsToLoad)}>{SelectionMenuUtilities.LABELS.APPLY_JOB}</Button>
+            // </div>
         )
     }
-}
 
 
 
