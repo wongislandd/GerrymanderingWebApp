957897387fd19bc883eb5aa9366b003baece4ffc
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-04-20 13:37:30 -0400

    A bunch of abstraction for building the convex hull

--- spring/src/main/java/cse416/spring/service/DatabaseWritingService.java
+++ spring/src/main/java/cse416/spring/service/DatabaseWritingService.java
@@ -4,6 +4,7 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import cse416.spring.helperclasses.ConcaveHullBuilder;
 import cse416.spring.helperclasses.EntityManagerSingleton;
 import cse416.spring.helperclasses.SinglePolygonGeoJSON;
 import cse416.spring.models.County;
@@ -86,8 +87,9 @@ public class DatabaseWritingService {
             String name = county.getString("name");
             JSONArray precinctKeys = county.getJSONArray("precincts");
             ArrayList<Precinct> precincts = getPrecinctObjectsFromKeys(precinctKeys, em);
-            JSONObject geometry = getConcaveGeometryOfPrecincts(precincts, gf, em);
-            County c = new County(Integer.parseInt(key), name, precincts, geometry);
+            Geometry hull = new ConcaveHullBuilder(precincts).getConcaveGeometryOfPrecincts();
+            JSONObject hullGeoJson = new SinglePolygonGeoJSON(hull.getCoordinates()).getJSON();
+            County c = new County(Integer.parseInt(key), name, precincts, hullGeoJson);
             em.persist(c);
         }
         /* Commit and close */
@@ -106,67 +108,7 @@ public class DatabaseWritingService {
     }
 
 
-    public static boolean isMultiPolygon(JSONArray coordinates) {
-        return coordinates.length() == 1;
-    }
-
-    /***
-     * Returns an array of coordinate objects
-     * @param coordinates
-     * @return A coordinate array from the entries in coordinates
-     */
-    public static Coordinate[] getCoordsFromJSONArray(JSONArray coordinates) {
-        Coordinate[] coords = new Coordinate[coordinates.length()];
-        /* For each coordinate that comprises the precinct*/
-        for (int j = 0; j < coordinates.length(); j++) {
-            JSONArray currentCoords = coordinates.getJSONArray(j);
-            coords[j] = new Coordinate(currentCoords.getDouble(0), currentCoords.getDouble(1));
-        }
-        return coords;
-    }
 
 
-    public static Geometry getConcaveHull(GeometryCollection gc) {
-        /* The threshhold may need to be dynamic? */
-        ConcaveHull ch = new ConcaveHull(gc, .12);
-        Geometry hull = ch.getConcaveHull();
-        return hull;
-    }
-
-    /***
-     * Used to take a collection of precincts and find the "rubber-band" geometry
-     * of the set of precinct geometries.
-     *
-     * @param precincts An array of precinct objects that belong to a county
-     * @param gf The geometry factory to create geometry with
-     * @param em The entity manager for access to the database
-     * @return A JSONObject which of a "feature" containing a single polygon geometry
-     */
-    public static JSONObject getConcaveGeometryOfPrecincts(ArrayList<Precinct> precincts, GeometryFactory gf, EntityManager em) {
-        /* Build a geometry collection in order to construct a concave hull object */
-        ArrayList<Geometry> geometries = new ArrayList<>();
-        /* For each precinct in the county */
-        for (int i = 0; i < precincts.size(); i++) {
-            JSONArray coordinates = precincts.get(i).retrieveCoordinates();
-            if (isMultiPolygon(coordinates)) {
-                /* Each entry in coordinates in a separate polygon, the separate polygon's will be treated as normal ones */
-                for (int k = 0; k < coordinates.length(); k++) {
-                    JSONArray currentPolygonCoords = coordinates.getJSONArray(k);
-                    geometries.add(gf.createPolygon(getCoordsFromJSONArray(currentPolygonCoords)));
-                }
-            } else {
-                geometries.add(gf.createPolygon(getCoordsFromJSONArray(coordinates)));
-            }
-        }
-        /* Create an array version to be funneled into a geometry collection */
-        Geometry[] geometryArr = new Geometry[geometries.size()];
-        geometryArr = geometries.toArray(geometryArr);
-        /* Using the geometry collection, construct the concave hull */
-        GeometryCollection gc = gf.createGeometryCollection(geometryArr);
-        Geometry hull = getConcaveHull(gc);
-        SinglePolygonGeoJSON geojson = new SinglePolygonGeoJSON(hull.getCoordinates());
-
-        return geojson.getJSON();
-    }
 
 }
