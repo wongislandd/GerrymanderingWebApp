cd9f788e0985edcc554b4b49b7c3c1d06472b7b0
Jihu Mun <jihu.mun@stonybrook.edu>
2021-04-22 22:46:17 -0400

    Add pop equality calculation

---
+++ spring/src/main/java/cse416/spring/helperclasses/constants/IdealPopulation.java
@@ -0,0 +1,19 @@
+package cse416.spring.helperclasses.constants;
+
+import cse416.spring.enums.StateName;
+
+import java.util.HashMap;
+
+public class IdealPopulation {
+    private static final HashMap<StateName, Integer> populationMap = new HashMap<>();
+
+    static {
+        populationMap.put(StateName.NORTH_CAROLINA, 461118);
+        populationMap.put(StateName.LOUISIANA, 0);
+        populationMap.put(StateName.TEXAS, 0);
+    }
+
+    public static int getIdealVoterPopulation(StateName stateName) {
+        return populationMap.get(stateName);
+    }
+}
