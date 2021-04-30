c4d52174f3ba6545d0a7531ff704f76214741925
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-04-30 01:41:57 -0400

    Built Districting geometry from precincts

--- 416-web-app/src/redux/reducers/rootReducer.js
+++ 416-web-app/src/redux/reducers/rootReducer.js
@@ -4,7 +4,7 @@ import * as ToolbarUtilities from "../../utilities/ToolbarUtilities";
 import * as MapUtilities from "../../utilities/MapUtilities";
 import TestGeneratedPlan from "../../data/NC/testGeneratedPlan.json";
 import EnactedDistrictingPlan2016 from '../../data/NC/EnactedDistrictingPlan2016WithData.json'
-import TestData from "../../data/NC/precincts_output.json"
+import TestData from "../../data/NC/TestData3.json"
 import React from "react";
 import Filter from "../../utilities/classes/Filter";
 import Job from "../../utilities/classes/models/Job";
