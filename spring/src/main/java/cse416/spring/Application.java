ce29188084ed5f7965770d2ff0fbe9b0ae74dfb5
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-04-29 23:31:38 -0400

    changed the dependency

--- spring/src/main/java/cse416/spring/Application.java
+++ spring/src/main/java/cse416/spring/Application.java
@@ -16,6 +16,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    // http://localhost:8080/swagger-ui.html
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
