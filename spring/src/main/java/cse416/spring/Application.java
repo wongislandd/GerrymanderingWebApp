8a2edabb3f866ead61c7e02557ebb433dae01ed3
Jihu Mun <jihu.mun@stonybrook.edu>
2021-04-29 23:24:25 -0400

    Fix dependencies

--- spring/src/main/java/cse416/spring/Application.java
+++ spring/src/main/java/cse416/spring/Application.java
@@ -12,12 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("cse416.spring")).build();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("cse416.spring")).build();
+    }
 }
